e51c2d6df86cfc5d56a0fe74ac936c2570a653c0
RTJ <rohittjob.it16@mnnit.ac.in>
2015-03-11 20:05:01 +0530

    Added custom filters for time

---
+++ src/forum/templatetags/time_short.py
@@ -0,0 +1,50 @@
+from datetime import datetime, timedelta
+from django import template
+from django.utils.timesince import timesince
+
+
+
+#####
+#####  This is to add custom filters regarding time remaining and since
+#####
+
+register = template.Library()
+
+@register.filter
+def until(value):
+	now = datetime.now()
+
+	try:
+		difference = value - now
+	except:
+		return value
+
+	if difference.days>0:
+		return str(difference.days)+' days'
+	else:
+		seconds = int(difference.total_seconds())
+		print seconds
+		hours = int(seconds/3600)
+		minutes = int(seconds/60)
+		if hours>0:
+			return str(hours)+' hours'
+		elif minutes>0:
+			return str(minutes)+ ' minutes'
+		else:
+			return str(seconds)+' seconds'
+
+
+
+
+
+@register.filter
+def age(value):
+    now = datetime.now()
+    try:
+        difference = now - value
+    except:
+        return value
+
+    if difference <= timedelta(minutes=1):
+        return 'just now'
+    return '%(time)s ago' % {'time': timesince(value).split(', ')[0]}
